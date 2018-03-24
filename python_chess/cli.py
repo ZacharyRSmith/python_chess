88eb879ce222730402966f383f9dfbe66ca879be
Zachary R. Smith <ZacharySmith4989@gmail.com>
2018-03-24 15:43:22 -0400

    test travis CI: purposefully leave lint complaint in

--- python_chess/cli.py
+++ python_chess/cli.py
@@ -8,6 +8,7 @@ import click
 @click.command()
 def main(args=None):
     """Console script for python_chess."""
+    foo = 0
     click.echo("Replace this message by putting your code into "
                "python_chess.cli.main")
     click.echo("See click documentation at http://click.pocoo.org/")
